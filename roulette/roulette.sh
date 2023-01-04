4b0ae423a78f298cbe07f6d0c391c37b4759ac01
piny4man <development@pinya.dev>
2023-01-05 00:16:44 +0100

    Add even logic of Iverse Labrouchete technique

--- roulette/roulette.sh
+++ roulette/roulette.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Colours
-greenColour="\e[0;32m\033[1m"
+ greenColour="\e[0;32m\033[1m"
 redColour="\e[0;31m\033[1m"
 blueColour="\e[0;34m\033[1m"
 yellowColour="\e[0;33m\033[1m"
@@ -18,19 +18,19 @@ function ctrl_c(){
 trap ctrl_c INT
 
 function helpPanel(){
-  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Usage: ${greenColour}$0${endColour}${endColour}\n"
-  echo -e "\t${purpleColour}-m]${endColour}${grayColour} Desired money to play${endColour}"
-  echo -e "\t${purpleColour}-t]${endColour}${grayColour} Roulette usage technique${endColour} ${purpleColour}(${endColour}${yellowColour}martingala${endColour}${purpleColour}/${endColour}${yellowColour}inverseLabrouchere${endColour}${purpleColour})${endColour}"
-  echo -e "\t${purpleColour}-h]${endColour}${grayColour} Show help panel${endColour}\n"
+  echo -e "\n${yellowColour}[+]${grayColour} Usage: ${greenColour}$0${endColour}\n"
+  echo -e "\t${purpleColour}-m]${grayColour} Desired money to play${endColour}"
+  echo -e "\t${purpleColour}-t]${grayColour} Roulette usage technique ${purpleColour}(${yellowColour}martingala${purpleColour}/${yellowColour}inverseLabrouchere${purpleColour})${endColour}"
+  echo -e "\t${purpleColour}-h]${grayColour} Show help panel${endColour}\n"
 }
 
 function martingala(){
-  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Current money:${endColour} ${greenColour}$money$ ${endColour}"
-  echo -ne "${yellowColour}[+]${endColour}${grayColour} How much money do you want to bet?${endColour}${turquoiseColour} -> ${endColour}" && read initialBet
-  echo -ne "${yellowColour}[+]${endColour}${grayColour} What type of bet do you want to do every time ${purpleColour}(${endColour}${yellowColour}odd${endColour}${purpleColour}/${endColour}${yellowColour}even${endColour}${purpleColour})${endColour}?${endColour} -> " && read oddEven
+  echo -e "\n${yellowColour}[+]${grayColour} Current money: ${greenColour}$money$ ${endColour}"
+  echo -ne "${yellowColour}[+]${grayColour} How much money do you want to bet?${turquoiseColour} -> ${endColour}" && read initialBet
+  echo -ne "${yellowColour}[+]${grayColour} What type of bet do you want to do every time ${purpleColour}(${yellowColour}odd${purpleColour}/${yellowColour}even${purpleColour})?${endColour} -> " && read oddEven
 
   # echo -e "\n${yellowColour}[+]${endColour}${grayColour} We are going to play with an initial amount of ${greenColour}$initialBet$ ${endColour}on ${purpleColour}$oddEven ${endCoulour} ${endColour}"
-  
+
   bet=$initialBet
   playCounter=1
   wrongPlays=""
@@ -42,13 +42,13 @@ function martingala(){
     # echo -e "\n${yellowColour}[+]${endColour}${grayColour} You bet ${greenColour}$bet${endColour}, your current money is ${greenColour}$money$ ${endColour}${endColour}"
     randomNumber="$(($RANDOM % 37))"
     # echo -e "\n${yellowColour}[+]${endColour}${grayColour} Current roulette number is ${blueColour}$randomNumber${endColour}${endColour}"
-    
+
     if [ "$money" -gt 0 ]; then
       if [ "$oddEven" == "even" ]; then
         if [ "$(($randomNumber % 2))" -eq 0 ]; then
           if [ "$randomNumber" -eq 0 ]; then
             # echo -e "${redColour}[@]${endColour}${grayColour} The number is ${redColour}0${endColour}, you ${endColour}${redColour}lose ${endColour}${grayColour}:(${endColour}"
-            bet=$(($bet*2)) 
+            bet=$(($bet*2))
             wrongPlays+="0 "
           else
             # echo -e "${turquoiseColour}[*]${endColour}${grayColour} The number is even, you ${endColour}${turquoiseColour}win${endColour}${grayColour}!${endColour}"
@@ -64,7 +64,7 @@ function martingala(){
           fi
         else
           # echo -e "${redColour}[@]${endColour}${grayColour} The number is odd, you ${endColour}${redColour}lose ${endColour}${grayColour}:(${endColour}"
-          bet=$(($bet*2)) 
+          bet=$(($bet*2))
           wrongPlays+="$randomNumber "
         fi
       else
@@ -80,16 +80,16 @@ function martingala(){
             maxBenefit=$money
           fi
         else
-          bet=$(($bet*2)) 
+          bet=$(($bet*2))
           wrongPlays+="$randomNumber "
         fi
       fi
     else
       echo -e "\n${redColour}[!] You lose all your money!${endColour}"
-      echo -e "${yellowColour}[!]${endColour}${grayColour} Total plays ${endColour}${turquoiseColour}$playCounter${endColour}"
-      echo -e "${yellowColour}[!]${endColour}${grayColour} Wrong consecutive plays:${endColour}"
+      echo -e "${yellowColour}[!]${grayColour} Total plays ${turquoiseColour}$playCounter${endColour}"
+      echo -e "${yellowColour}[!]${grayColour} Wrong consecutive plays:${endColour}"
       echo -e "${turquoiseColour}[ $wrongPlays]${endColour}"
-      echo -e "${yellowColour}[!]${endColour}${grayColour} Maximum benefit earned ${greenColour}$maxBenefit$ ${endColour} ${endColour}"
+      echo -e "${yellowColour}[!]${grayColour} Maximum benefit earned ${greenColour}$maxBenefit$ ${endColour}"
       tput cnorm; exit 0
     fi
 
@@ -100,46 +100,50 @@ function martingala(){
 }
 
 function inverseLabrouchere(){
-  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Current money:${endColour} ${greenColour}$money$ ${endColour}"
-  echo -ne "${yellowColour}[+]${endColour}${grayColour} What type of bet do you want to do every time ${purpleColour}(${endColour}${yellowColour}odd${endColour}${purpleColour}/${endColour}${yellowColour}even${endColour}${purpleColour})${endColour}?${endColour} -> " && read oddEven
-  
-  declare -a betSequence=(1 2 3 4)
-  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Starting with ${endColour}${blueColour}[${betSequence[@]}]${endColour}${grayColour} sequence${endColour}"
-  
-  bet=$((${betSequence[0]} + ${betSequence[-1]}))
-  unset betSequence[0]
-  unset betSequence[-1]
-  betSequence=(${betSequence[@]})
+  echo -e "\n${yellowColour}[+]${grayColour} Current money: ${greenColour}$money$ ${endColour}"
+  echo -ne "${yellowColour}[+]${grayColour} What type of bet do you want to do every time ${purpleColour}(${yellowColour}odd${purpleColour}/${yellowColour}even${purpleColour})?${endColour} -> " && read oddEven
 
-  echo -e "${yellowColour}[+]${endColour}${grayColour} Bet${endColour}${greenColour} $bet$ ${endColour}${grayColour}and our secuence decrease to ${endColour}${blueColour}[${betSequence[@]}]${endColour}"
+  declare -a betSequence=(1 2 3 4)
+  echo -e "\n${yellowColour}[+]${grayColour} Starting with ${blueColour}[${betSequence[@]}]${grayColour} sequence${endColour}"
+
+  bet=$((${betSequence[0]} + ${betSequence[-1]}))
+  money=$(($money-$bet))
+  betSequence=(${betSequence[@]})
 
   tput civis
   while true; do
     randomNumber="$(($RANDOM % 37))"
-    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Current roulette number is ${blueColour}$randomNumber${endColour}${endColour}"
+    money=$(($money-$bet))
+    echo -e "\n${yellowColour}[+]${grayColour} Current roulette number is ${blueColour}$randomNumber${endColour}"
+    echo -e "${yellowColour}[+]${grayColour} Your current money is ${greenColour}$money$ ${endColour}"
+    echo -e "${yellowColour}[+]${grayColour} Bet${greenColour} $bet$ ${endColour}"
 
     if [ "$oddEven" == "even" ]; then
-       if [ "$(($randomNumber % 2))" -eq 0 ]; then
-        if [ "$randomNumber" -eq 0 ]; then
-          # echo -e "${redColour}[@]${endColour}${grayColour} The number is ${redColour}0${endColour}, you ${endColour}${redColour}lose ${endColour}${grayColour}:(${endColour}"
-          bet=$(($bet*2)) 
-          wrongPlays+="0 "
+      if [ "$(($randomNumber % 2))" -eq 0 ] && [ "$randomNumber" -ne 0 ]; then
+        reward=$(($bet*2))
+        let money+=$reward
+        echo -e "${yellowColour}[+]${grayColour} Your current money is ${greenColour}$money$ ${endColour}"
+        betSequence+=($bet)
+        betSequence=(${betSequence[@]})
+        if [ "${#betSequence[@]}" -gt 1 ]; then
+          bet=$((${betSequence[0]} + ${betSequence[-1]}))
+        elif ["${#betSequence[@]}" -eq 1]; then
+          bet=${betSequence[0]}
         else
-          # echo -e "${turquoiseColour}[*]${endColour}${grayColour} The number is even, you ${endColour}${turquoiseColour}win${endColour}${grayColour}!${endColour}"
-          reward=$(($bet*2))
-          # echo -e "${purpleColour}[!]${endColour}${grayColour} You win a total of ${endColour}${greenColour}$reward$ ${endColour}"
-          money=$(($money+$reward))
-          # echo -e "${purpleColour}[!]${endColour}${grayColour} You have ${endColour}${greenColour}$money$ ${endColour}"
-          bet=$initialBet
-          wrongPlays=""
-          if [ "$money" -gt "$maxBenefit" ]; then
-            maxBenefit=$money
-          fi
+          betSequence=(1 2 3 4)
         fi
       else
-        # echo -e "${redColour}[@]${endColour}${grayColour} The number is odd, you ${endColour}${redColour}lose ${endColour}${grayColour}:(${endColour}"
-        bet=$(($bet*2)) 
-        wrongPlays+="$randomNumber "
+        echo "El numero es impar, pierdes"
+        unset betSequence[0]
+        unset betSequence[-1] 2>/dev/null
+        betSequence=(${betSequence[@]})
+        if [ "${#betSequence[@]}" -gt 1 ]; then
+          bet=$((${betSequence[0]} + ${betSequence[-1]}))
+        elif ["${#betSequence[@]}" -eq 1]; then
+          bet=${betSequence[0]}
+        else
+          betSequence=(1 2 3 4)
+        fi
       fi
     fi
 
